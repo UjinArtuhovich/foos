c39ca31e756b16bbbb8162e03963f32b776748f1
Ujin <ujin_jhon@mail.ru>
2022-08-02 16:46:36 +0300

    Initial Commit

---
+++ foos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foos
+//
+//  Created by Ujin Artuhovich on 2.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
